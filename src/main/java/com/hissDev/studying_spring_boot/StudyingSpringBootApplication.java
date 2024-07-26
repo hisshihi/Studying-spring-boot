6fac866a7d5c2e49007a23f154f814bc973a2e90
HissDev <hissoffc@gmail.com>
2024-07-27 00:50:28 +0300

    first commit

---
+++ src/main/java/com/hissDev/studying_spring_boot/StudyingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hissDev.studying_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyingSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hissDev/studying_spring_boot/StudyingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hissDev.studying_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
