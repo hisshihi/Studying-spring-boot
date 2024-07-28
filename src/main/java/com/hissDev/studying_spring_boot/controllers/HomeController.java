86ecd78fce8eb7514ae5146815dc79deb1159133
HissDev <hissoffc@gmail.com>
2024-07-28 15:44:22 +0300

    Добавил несколько тестовых методов контроллера.

--- src/main/java/com/hissDev/studying_spring_boot/controllers/HomeController.java
+++ src/main/java/com/hissDev/studying_spring_boot/controllers/HomeController.java
@@ -1,7 +1,21 @@
 package com.hissDev.studying_spring_boot.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class HomeController {
 
+    @GetMapping
+    public String sayHello() {
+        return "Welcome to Spring Boot!";
+    }
 
+    @GetMapping("about")
+    public String about() {
+        return "This is about Spring Boot!";
+    }
 
 }
